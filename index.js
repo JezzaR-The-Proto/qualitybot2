fe3c495dec48be432cd66a1555c55e697033a672
Jeremy Russ <jeremyruss43@gmail.com>
2019-08-02 22:46:40 +0100

    make it better

--- index.js
+++ index.js
@@ -37,28 +37,4 @@ client.on('kick', message => {
   }
 })
 
-client.on('ban', message => {
-  if (!message.guild) return;
-  if (message.content.startsWith('!ban')) {
-    const user = message.mentions.users.first();
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-        member.ban({
-          reason: 'They were bad!',
-        }).then(() => {
-          message.reply(`Successfully banned ${user.tag}`);
-        }).catch(err => {
-          message.reply('I was unable to ban the member');
-          console.error(err);
-        });
-      } else {
-        message.reply('That user isn\'t in this guild!');
-      }
-    } else {
-      message.reply('You didn\'t mention the user to ban!');
-    }
-  }
-});
-
 client.login(process.env.BOT_TOKEN)
